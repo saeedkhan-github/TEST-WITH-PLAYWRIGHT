699dc48b62b1893b474f5aa8b85243309d7875e7
Saeed Ullah <saeedullah321@gmail.com>
2024-11-11 12:53:10 +0500

    testing

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -3,14 +3,24 @@ import {test,expect} from "@playwright/test"
 test.describe("Validate Login Functionality",()=>{
 
     test.beforeEach(async({page})=>{
-        await page.goto('http://zero.webappsecurity.com/login.html')
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     })
-    test('Validate Login with invalid credentials ',async({page})=>{
-        await page.locator('#user_login').fill('Admin');
-        await page.locator('#user_password').fill('Admin123');
-        await page.locator('input[value="Sign in"]').click();
-        await page.getByText('Login and/or password are wrong.').isVisible
-        await expect(page.locator('.alert.alert-error')).toHaveText('Login and/or password are wrong.');
+    test.fixme('Verify Login with invalid credentials ',async({page})=>{
+        await page.locator('input[placeholder="Username"]').fill('user');
+        await page.locator('input[placeholder="Password"]').fill('usera123');
+        await page.locator('button[type="submit"]').click();
+        // await page.getByText('div[role="alert"]').isVisible
+        await expect(page.locator('div[role="alert"]')).toHaveText('Invalid credentials');
     })
+    test('Verify successful Login with valid credentials and logout ',async({page})=>{
+        await page.locator('input[placeholder="Username"]').fill('Admin');
+        await page.locator('input[placeholder="Password"]').fill('admin123');
+        await page.locator('button[type="submit"]').click();
+        await page.waitForLoadState();
+        await page.locator('.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon').click();
+        await page.locator('oxd-userdropdown-link').filter({hasText:'Logout'}).click();
+    })
+
+    
 
 })
