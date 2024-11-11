0d8bec5cf428cc7549e0022614198b930557537e
Saeed Ullah <saeedullah321@gmail.com>
2024-11-11 22:28:28 +0500

    Login Page Object Completed

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,26 +1,26 @@
 import {test,expect} from "@playwright/test"
+import { LoginPage } from "../../page/LoginPage"
 
 test.describe("Validate Login Functionality",()=>{
-
+    let loginPage : LoginPage;
     test.beforeEach(async({page})=>{
+        loginPage= new LoginPage(page);
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        await page.waitForLoadState();
     })
-    test.fixme('Verify Login with invalid credentials ',async({page})=>{
-        await page.locator('input[placeholder="Username"]').fill('user');
-        await page.locator('input[placeholder="Password"]').fill('usera123');
-        await page.locator('button[type="submit"]').click();
-        // await page.getByText('div[role="alert"]').isVisible
+    test.afterEach(async({page})=>{
+        page.close();
+    })
+    test('Verify Login with invalid credentials ',async({page})=>{
+        await loginPage.Login('Admin11','admin123')
         await expect(page.locator('div[role="alert"]')).toHaveText('Invalid credentials');
     })
     test('Verify successful Login with valid credentials and logout ',async({page})=>{
-        await page.locator('input[placeholder="Username"]').fill('Admin');
-        await page.locator('input[placeholder="Password"]').fill('admin123');
-        await page.locator('button[type="submit"]').click();
-        await page.waitForLoadState();
-        await page.locator('.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon').click();
-        await page.locator('oxd-userdropdown-link').filter({hasText:'Logout'}).click();
-    })
+        await loginPage.Login('Admin','admin123')
+        
+        await loginPage.Logout();
+        await expect(page.url()).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-    
+    })
 
 })
