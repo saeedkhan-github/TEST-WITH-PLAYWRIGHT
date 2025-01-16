8cffb1767650faca7b7718ecbc3223e34c4c46c9
Saeed Ullah <saeedullah321@gmail.com>
2025-01-16 20:56:42 +0500

    User search test case added on admin page

--- page/AdminPage.ts
+++ page/AdminPage.ts
@@ -1,6 +1,41 @@
-import { expect, Locator, Page} from '@playwright/test';
+import { Page , Locator} from '@playwright/test';
 
-export class AdminPage{
+export class AdminPage {
+  readonly page: Page;
 
-    
+  // Locators
+
+  readonly searchInputField: Locator;
+  readonly submitSearch: Locator;
+
+
+  constructor(page: Page) {
+    this.page = page;
+    this.searchInputField= page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]')
+  }
+
+  // Navigate to Admin Page
+  async getSearchInputLocator(name:string) {
+     this.searchInputField.fill(name);
+  }
+
+  // Add a new user
+//   async addUser(
+//     username: string,
+//     userRole: string,
+//     employeeName: string,
+//     status: string
+//   ) {
+//     await this.addUserButton.click();
+//     await this.usernameInput.fill(username);
+//     await this.userRoleDropdown.selectOption(userRole); // e.g., '1' for Admin
+//     await this.employeeNameInput.fill(employeeName);
+//     await this.statusDropdown.selectOption(status); // e.g., '1' for Enabled
+//     await this.saveButton.click();
+//   }
+
+  // Get success message
+//   async getSuccessMessage() {
+//     return await this.successToast.innerText();
+//   }
 }
--- tests/e2e/e2e-admin.spec.ts
+++ tests/e2e/e2e-admin.spec.ts
@@ -2,12 +2,15 @@
 import {test,expect} from "@playwright/test"
 import { LoginPage } from "../../page/LoginPage"
 import { LeftMenu } from "../../page/LeftMenu";
+import { AdminPage } from "../../page/AdminPage";
 test.describe('Admin Page Tests',()=>{
     let loginPage : LoginPage;
     let list: LeftMenu;
+    let admin: AdminPage;
     test.beforeEach(async({page})=>{
         loginPage= new LoginPage(page);
         list= new LeftMenu(page);
+        admin= new AdminPage(page);
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         await loginPage.Login('Admin','admin123')
     })
@@ -16,8 +19,16 @@ test.describe('Admin Page Tests',()=>{
         let dashboardText =await page.locator('.oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module').innerText();
         await expect(dashboardText).toBe('Dashboard')
         await (await list.ListItem('Admin')).click(); // pass any left menu item,admin,PIM, Leave, Time, to click on the item
-       // await page.locator('.oxd-main-menu-item:has-text("Admin")').click();
         expect(await page.locator('.oxd-topbar-header-title').textContent()).toContain('User Management')
-        // await page.locator('.oxd-main-menu-item.active.toggle').click();
+        // await page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]').fill('Zyed');
+        await admin.getSearchInputLocator('zyed')
+        await page.locator('button[type="submit"]').click();
+        await page.waitForSelector('#oxd-toaster_1');
+        let toastmsg= await page.locator('#oxd-toaster_1').textContent();
+        expect(toastmsg).toContain('No Records Found');
+
     })
 })
+
+
+//#oxd-toaster_1 id for toast message no record 
