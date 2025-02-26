fc2e24561b7b3620897d6bff4a75f4713c034803
Saeed Ullah <saeedullah321@gmail.com>
2025-02-26 17:54:37 +0500

    Reporting fixs,  Loginpage fix

--- page/LoginPage.ts
+++ page/LoginPage.ts
@@ -15,7 +15,7 @@ export class LoginPage {
   }
 
   async Login(username: string, password: string) {
-    // await this.userinput.waitFor({ state: 'visible' });
+    await this.userinput.waitFor({ state: 'visible' });
     await this.userinput.fill(username);
     await this.passwordinput.fill(password);
     await this.submitButton.click();
