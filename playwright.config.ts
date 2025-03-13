4db33177987f258aa72b5da62a57037faeb269f5
Saeed Ullah <saeedullah321@gmail.com>
2025-03-14 01:47:29 +0500

    merge build into reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const authFile = path.join(__dirname, 'tests/.auth/user.json');
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './build/tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
