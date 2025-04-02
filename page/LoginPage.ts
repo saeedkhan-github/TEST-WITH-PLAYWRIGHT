623f3e276e28e46b4e7422eb621ca92947dff8ed
Saeed Ullah <saeedullah321@gmail.com>
2025-04-02 13:51:56 +0500

    fix loading state

--- page/LoginPage.ts
+++ page/LoginPage.ts
@@ -15,7 +15,10 @@ export class LoginPage {
   }
 
   async Login(username: string, password: string) {
-    await this.userinput.waitFor({ state: 'visible' });
+    await this.page.waitForLoadState('networkidle');
+    await this.page.waitForSelector('input[placeholder="Username"]', { state: 'visible' });
+    await this.page.waitForSelector('input[placeholder="Password"]', { state: 'visible' });
+    // await this.userinput.waitFor({ state: 'visible' });
     await this.userinput.fill(username);
     await this.passwordinput.fill(password);
     await this.submitButton.click();
