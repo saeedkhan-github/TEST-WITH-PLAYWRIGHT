1b19b05c94e2393ae7efeaa5b9df46c1efee6ed6
Saeed Ullah <saeedullah321@gmail.com>
2025-04-05 19:45:42 +0500

    add webhook for build, to run on jenkins, expose localhost:8080 by ngrok

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,24 +12,18 @@ const testDir = process.env.CI ? './build/tests' : './tests';
 export default defineConfig({
   testDir,
   timeout: 30 *1000,
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2: undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-           
   reporter: [['html'] ,
              ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+    
     baseURL: process.env.BASE_URL,
     ignoreHTTPSErrors: true,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    actionTimeout: 15 * 1000,
     trace: 'retain-on-failure',
   },
 
